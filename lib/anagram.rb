58972f95589d47b5ace77c300319fccc5e35f01e
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 12:42:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
 
   def match(new_string)
     new_string.each do |word|
-      if word.match(string)
+      if word.include? string
         return word
       else
         return []

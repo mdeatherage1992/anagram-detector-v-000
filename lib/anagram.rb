05598ba9f28045cdb877dbcbde2f100316943622
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 13:08:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,11 +7,12 @@ class Anagram
   end
 
   def match(new_string)
+  array = []
   new_string.each do |word|
     string1 = word.split('')
     string2 = string.split('')
     if string1.sort == string2.sort && string1.length == string2.length
-      return word
+      array << word
     else
     end
   end

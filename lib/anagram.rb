2e7041b91c4d1c334026c680189d464c8147c7e8
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 12:39:59 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def self.match(new_string)
+    anagram_string = new_string.split(' ')
+    anagram_string.each do |word|
+      if word.match(string)
+        return word
+      else
+        return []
+      end
+    end
+  end
+
+end
+      
+      

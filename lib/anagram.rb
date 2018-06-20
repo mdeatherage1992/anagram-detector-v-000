f0088b846b8ee0eb7addaaa658bf55650999aac3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 13:08:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(new_string)
+  array = []
+  new_string.each do |word|
+    string1 = word.split('')
+    string2 = string.split('')
+    if string1.sort == string2.sort && string1.length == string2.length
+      array << word
+    else
+    end
+  end
+  return array
+end
+
+
+
+
+end

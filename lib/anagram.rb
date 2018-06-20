3d4d3f13dc9df65166e74745620cb03c23ed22a9
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-20 13:06:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,10 +10,7 @@ class Anagram
   new_string.each do |word|
     string1 = word.split('')
     string2 = string.split('')
-    if string1.length == string2.length && string1.include? string2
-      return word
-    else
-      return []
+    if string1.include? string2 && string1.length == string2.length
     end
   end
 end
